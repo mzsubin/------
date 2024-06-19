050622be4d453032e8316c74ae72da15627f7fe9
mzsubin <subin@mz.com>
2024-06-19 18:10:35 +0900

    commit and push

--- webapp/Component.js
+++ webapp/Component.js
@@ -10,11 +10,7 @@ sap.ui.define(
     return UIComponent.extend("ui5.walkthrough.Component", {
       metadata: {
         interfaces: ["sap.ui.core.IAsyncContentCreation"],
-        rootView: {
-          viewName: "ui5.walkthrough.view.App",
-          type: "XML",
-          id: "app",
-        },
+        manifest: "json",
       },
 
       init() {
@@ -23,17 +19,30 @@ sap.ui.define(
         // set data model
         const oData = {
           recipient: {
-            name: "World",
+            name: "World", //초기값지정
+          },
+          recipient2: {
+            name: "World2",
           },
         };
         const oModel = new JSONModel(oData);
         this.setModel(oModel);
 
-        // set i18n model
-        const i18nModel = new ResourceModel({
-          bundleName: "ui5.walkthrough.i18n.i18n",
-        });
-        this.setModel(i18nModel, "i18n");
+        // // //2번째 패널
+        // const oData2 = {
+        //   recipient2: {
+        //     name2: "World2",
+        //   },
+        // };
+
+        // const oModel2 = new JSONModel(oData2);
+        // this.setModel(oModel2);
+
+        // // set i18n model
+        // const i18nModel = new ResourceModel({
+        //   bundleName: "ui5.walkthrough.i18n.i18n",
+        // });
+        // this.setModel(i18nModel, "i18n");
       },
     });
   }
--- webapp/index.js
+++ webapp/index.js
@@ -4,6 +4,19 @@
 // 	alert("UI5 is ready");
 // 	/* eslint-enable no-alert */
 // // });
-sap.ui.define(["sap/ui/core/mvc/XMLView"], 
-(XMLView) => { "use strict";
- XMLView.create({viewName: "ui5.walkthrough.view.App"}).then((oView) => oView.placeAt("content"));});
+
+// sap.ui.define(["sap/ui/core/mvc/XMLView"],
+// (XMLView) => { "use strict";
+//  XMLView.create({viewName: "ui5.walkthrough.view.App"}).then((oView) => oView.placeAt("content"));});
+
+sap.ui.define(["sap/ui/core/ComponentContainer"], (ComponentContainer) => {
+  "use strict";
+
+  new ComponentContainer({
+    name: "ui5.walkthrough",
+    settings: {
+      id: "walkthrough",
+    },
+    async: true,
+  }).placeAt("content");
+});
